980b2142b04b9a857861ed66916437c1eb872b0c
Yavuz CIRIT <yavuzcirit23@gmail.com>
2025-12-28 00:43:15 +0300

    bugfix: erased unnecessary code blocks
    Removed comments related to user tracking initialization and logging.

--- plugins/userTracking.client.ts
+++ plugins/userTracking.client.ts
@@ -1,13 +1,10 @@
 import { logger } from '~/utils/logger'
 
 export default defineNuxtPlugin(() => {
-  // Initialize user tracking
   const { getUserId } = useUserTracking()
   
-  // Set user tracking ID getter in logger
   logger.setUserTrackingIdGetter(getUserId)
   
-  // Log user session start
   if (import.meta.client) {
     logger.info('User session started', {
       userAgent: navigator.userAgent,
